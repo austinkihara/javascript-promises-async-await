e6dea3ce05c48ff51989f31ad8feec61bc6f20fa
austin kihara <austin.kihara@graduway.com>
2021-09-25 18:06:19 -0700

    second module 4

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-import { fetchWithTimeout } from './services'
+import { fetchMovies, fetchBooks, fetchWithTimeout } from './services'
 
 let movies = require('./data/movies.json')
 
-export function fetchMovies() {
-  const resolveFunction = () => movies;
-  return fetchWithTimeout(1000).then(resolveFunction)
+// export function fetchMovies() {
+//   const resolveFunction = () => movies;
+//   return fetchWithTimeout(1000).then(resolveFunction)
+// }
+
+// const moviePromise = fetchMovies()
+
+// moviePromise.then((results) => {
+//   console.log(results)
+// })
+
+export function getBooksAndMovies() {
+  Promise.all([fetchBooks(), fetchMovies()])
+  .then(([books, movies ]) => ({
+    books,
+    movies
+  }))
+  .catch(error => console.log("Error fetching books and movies", error));
 }
 
-let moviePromise = fetchMovies()
+const getBooksAndMoviesPromise = getBooksAndMovies()
 
-moviePromise.then((results) => {
+getBooksAndMoviesPromise.then((results) => {
   console.log(results)
 })
