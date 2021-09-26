23d0c40608b31c34cd503a69cc3456395dbfecd1
austin kihara <austin.kihara@graduway.com>
2021-09-26 12:31:42 -0700

    second module 9

--- src/index.js
+++ src/index.js
@@ -77,3 +77,12 @@ getBooksOrMoviesAsync()
   .catch(error => {
     console.log(error)
   })
+
+  const timer1 = setTimeout(() => {
+    console.log("timer 1 has finished")
+  }, 3000)
+
+  const timer2 = setTimeout(() => {
+    console.log("timer 2 has finished")
+    clearTimeout(timer1)
+  }, 2000)
