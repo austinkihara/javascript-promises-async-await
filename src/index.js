bada2bf30e5d6978f73819f3dc58755f9ad4406b
austin kihara <austin.kihara@graduway.com>
2021-09-25 18:13:26 -0700

    second module 4

--- src/index.js
+++ src/index.js
@@ -13,17 +13,30 @@ let movies = require('./data/movies.json')
 //   console.log(results)
 // })
 
-export function getBooksAndMovies() {
-  Promise.all([fetchBooks(), fetchMovies()])
-  .then(([books, movies ]) => ({
-    books,
-    movies
-  }))
-  .catch(error => console.log("Error fetching books and movies", error));
+const getBooksAndMovies = () => {
+  return Promise.all([fetchBooks(), fetchMovies()])
+    .then(([books, movies ]) => ({
+      books,
+      movies
+    }))
+    .catch(error => console.log("Error fetching books and movies", error));
 }
 
 const getBooksAndMoviesPromise = getBooksAndMovies()
 
 getBooksAndMoviesPromise.then((results) => {
-  console.log(results)
+  console.log('getBooksAndMoviesPromise', results)
+})
+
+
+const getBooksOrMovies = () => {
+  return Promise.race([fetchBooks(), fetchMovies()])
+    .then(results => results)
+    .catch(error => console.log("Error waiting for the promise race", error))
+}
+
+const getBooksOrMoviesPromise = getBooksOrMovies()
+
+getBooksOrMoviesPromise.then((results) => {
+  console.log('getBooksOrMoviesPromise', results)
 })
